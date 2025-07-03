d127df71be4cd69231861c5427daeab2ac09eb27
Gabriel Borges <gabrielsmborges@gmail.com>
2025-07-04 00:57:13 +0100

    fix: imports

--- apps/web/src/app/(secure)/(standalone)/layout.tsx
+++ apps/web/src/app/(secure)/(standalone)/layout.tsx
@@ -1,8 +1,4 @@
 import { Layout } from '@repo/ui/layout'
-import HeaderTitle from '@/components/header/HeaderTitle'
-import { BDIcon } from '@/assets/icons'
-import Link from 'next/link'
-import { ROUTES } from '@/constants/routes'
 
 const StandaloneLayout = async ({
   children
@@ -10,13 +6,6 @@ const StandaloneLayout = async ({
   return (
     <Layout>
       <Layout.Main>
-        <Layout.MainHeader className="flex flex-row items-center gap-4">
-          <Link href={ROUTES.DASHBOARD}>
-            <BDIcon iconName="arrowLeft" className="h-6 w-6" />
-          </Link>
-          <HeaderTitle />
-        </Layout.MainHeader>
-
         <Layout.MainContent>{children}</Layout.MainContent>
       </Layout.Main>
     </Layout>
